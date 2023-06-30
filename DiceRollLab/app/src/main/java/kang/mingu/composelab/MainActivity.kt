8c2c79150b365b3be5b7e88f6ec509993d129b52
kangmin1012 <kangmin1012@naver.com>
2023-06-30 17:27:26 +0900

    DiceRoller CodeLab 따라하기

--- DiceRollLab/app/src/main/java/kang/mingu/composelab/MainActivity.kt
+++ DiceRollLab/app/src/main/java/kang/mingu/composelab/MainActivity.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
